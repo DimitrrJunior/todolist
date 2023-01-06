55bdff19acfbe8ca0cdc16a62349a6497e22b281
--Dmitriy <dpemvr@yandex.ru>
2023-01-06 20:28:56 +0500

    Adddddddd

--- js/script.js
+++ js/script.js
@@ -4,48 +4,53 @@
 const wordInputNode = document.querySelector('.word__input');
 const wordBtnNode = document.querySelector('#button-add');
 const todoBoxNode = document.querySelector('.todo__box');
+const deleteBtn = document.querySelector('.todo-clear');
+const successfullyBtnNode = document.querySelector('.successfully');
 let todoList = [];
 
-wordBtnNode.addEventListener('click', function () {
-     event.preventDefault();
-     if (wordInputNode.value == '') {
-          alert('Введите задачу на день в поле ввода');
-     } else {
-          let newTodo = {
-               todo: wordInputNode.value,
-               important: false
-          };
-          todoList.push(newTodo);
-          displayMessages();
-     }
+{
+     wordBtnNode.addEventListener('click', function () {
+          event.preventDefault();
      
-});
-
-function displayMessages() {
-     let todoBoxinnerblock = '';
-     todoList.forEach(function (item) {
-          todoBoxinnerblock += `
-          <div class="todo__box--item">
-               <button class="successfully">
-                    
-               </button> 
-          <p class="todo__box--text">
-               ${item.todo}
-          </p>
-          </div>`;
-          todoBoxNode.innerHTML = todoBoxinnerblock;
+          if (wordInputNode.value == '') {
+               alert('Введите задачу на день в поле ввода');
+          } else {
+               let newTodo = {
+                    todo: wordInputNode.value,
+                    important: false
+               };
+               todoList.push(newTodo);
+               displayMessages();
+               wordInputNode.value = '';
+          };
      });
-     const deleteBtn = document.querySelector('.todo-clear');
+
+     function displayMessages() {
+          let todoBoxinnerblock = '';
+          todoList.forEach(function (item) {
+               todoBoxinnerblock += `
+               <div class="todo__box--item">
+                    <label class="container">
+                         <input type="checkbox" checked="checked" class="check-highload">
+                         <span class="highload2"></span>
+                    </label>
+                    <p class="todo__box--text">
+                      ${item.todo}
+                    </p>
+               </div>`;
+               todoBoxNode.innerHTML = todoBoxinnerblock;
+          });
+     };
+
      deleteBtn.addEventListener('click', function () {
           todoList = [];
           todoBoxNode.innerHTML = '';
-     })
-};
+     });
+}
 
 
-// const successfullyBtnNode = document.querySelector('.successfully');
-// successfullyBtnNode.addEventListener('click', function () {
-//      successfullyBtnNode.innerHTML = `<i class="fa-solid fa-check ic-sett"></i>`;
-// });
+
+
+
 
 
